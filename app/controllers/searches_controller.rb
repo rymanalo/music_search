6dcccb0680ec12478c4af6287c3b805a2329c0a7
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-20 13:24:23 -0700

    Fix no image bug.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -12,14 +12,18 @@ class SearchesController < ApplicationController
     @query = params[:q]
     @response = Search.new(@query)
 
-    if @response.check_if_artist_nil
-      redirect_to :action => 'sorry', :q => params[:q]
-    else
+    # if @response.check_if_artist_nil
+    #   redirect_to :action => 'sorry', :q => params[:q]
+    # else
       @response.artist_bio
       @response.artist_top_tracks
       array = @response.top_videos_youtube_ids
       @top_video = array.shift
       @top_video_playlist = array.join(",")
+    if @response.check_if_image_nil
+      @artist_image = true
+    else
+      @artist_image = false
       @response.artist_image
     end
   end
