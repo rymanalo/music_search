8664ed36332722a93bbb59818d59b71431cf1b99
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-21 23:30:14 -0700

    Change mailer in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,10 +9,10 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "12manalo@gmail.com"
+  # config.mailer_sender = "12manalo@gmail.com"
 
   # Configure the class responsible to send e-mails.
-  config.mailer = "Devise::Mailer"
+  # config.mailer = "Devise::Mailer"
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
