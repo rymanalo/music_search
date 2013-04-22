a815c14d5da1a9ffa64e92a153a2eb335905a272
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-21 19:24:40 -0700

    Add facebook route.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,9 @@ class SearchesController < ApplicationController
   before_filter :authenticate_user!, :only => [:facebook]
 
   def index
+    if user_signed_in?
+      @current_user = current_user
+    end
   end
 
   def sorry
@@ -20,11 +23,7 @@ class SearchesController < ApplicationController
       array = @response.top_videos_youtube_ids
       @top_video = array.shift
       @top_video_playlist = array.join(",")
-
-
       if @response.check_if_image_nil
-
-
         @artist_image = true
       else
         @artist_image = false
@@ -33,21 +32,10 @@ class SearchesController < ApplicationController
     end
   end
 
-  def friends
-    if user_signed_in?
-      @current_user = current_user
-    end
-  end
-
   def facebook
     @current_user = current_user
     @graph = Koala::Facebook::API.new(@current_user.oauth_token)
-
-    @profile = @graph.get_object("me")
-    @friends = @graph.get_connections("me", "friends")
-
-    #@friends = @graph.get_connections("me", "friends", :fields => "picture,name,birthday,hometown,location")[0..20]
-    friend_count = params[:friend_count].to_i
+    @profile_pic = @graph.get_object("me?fields=picture")['picture']['data']['url']
     @music = @graph.get_connections("me", "music?fields=name")
   end
 
